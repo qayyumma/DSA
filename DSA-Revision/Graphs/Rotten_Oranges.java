69a813599a9e004e04d406c00f7f18a5b4eeafb3
qayyumma <qayyumemailid@gmail.com>
2024-11-02 00:12:05 +0530

    Update Rotten_Oranges.java

--- DSA-Revision/Graphs/Rotten_Oranges.java
+++ DSA-Revision/Graphs/Rotten_Oranges.java
@@ -1,125 +1,69 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Solution {
 
-    class cell{
+    // Inner class to represent a cell's coordinates in the grid
+    class Cell {
+        int row;
+        int col;
 
-        int i;
-
-        int j;
-
-        public cell(int i, int j){
-
-            this.i = i;
-
-            this.j = j;
-
+        public Cell(int row, int col) {
+            this.row = row;
+            this.col = col;
         }
-
     }
 
-    public boolean rotent(int i, int j, int N, int M, int[][] A){
-
-        if(i < 0 || j < 0 || i >= N || j >= M || A[i][j] != 1 ){
-
-            return false;
-        }
-
-        return true;
-
+    // Method to check if a cell contains a fresh orange within the grid bounds
+    public boolean canRot(int row, int col, int rows, int cols, int[][] grid) {
+        return row >= 0 && col >= 0 && row < rows && col < cols && grid[row][col] == 1;
     }
 
-    public int solve(int[][] A) {
-
-        Queue<cell> q = new LinkedList<>();
-
-        int N = A.length;
-
-        int M = A[0].length;
-
-        int totalone = 0;
-
+    public int solve(int[][] grid) {
+        Queue<Cell> queue = new LinkedList<>();
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int freshOranges = 0;
         int time = 0;
 
-        for(int i = 0; i < N; i++){
-
-            for(int j = 0; j < M; j++){
-
-                if(A[i][j]==2){
-
-                    q.add(new cell (i, j));
-
+        // Initialize queue with all rotten oranges and count fresh oranges
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 2) {
+                    queue.add(new Cell(i, j)); // Add rotten oranges to queue
+                } else if (grid[i][j] == 1) {
+                    freshOranges++; // Count fresh oranges
                 }
-                else if(A[i][j]==1){
-                    totalone++;
+            }
+        }
 
+        // Directions for neighboring cells (left, right, up, down)
+        int[][] directions = { {0, -1}, {0, 1}, {-1, 0}, {1, 0} };
+
+        // BFS to rot adjacent oranges
+        while (!queue.isEmpty() && freshOranges > 0) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                Cell rotten = queue.remove();
+
+                for (int[] direction : directions) {
+                    int newRow = rotten.row + direction[0];
+                    int newCol = rotten.col + direction[1];
+
+                    if (canRot(newRow, newCol, rows, cols, grid)) {
+                        grid[newRow][newCol] = 2; // Make the fresh orange rotten
+                        queue.add(new Cell(newRow, newCol));
+                        freshOranges--;
+                    }
                 }
-
             }
 
+            // Increment time after each level (one minute)
+            time++;
         }
 
-        while(q.size() > 0){
-
-            int size =  q.size();
-
-            for(int i = 0; i < size; i++){
-
-            cell c = q.remove();
-
-            if( rotent(c.i, c.j-1, N, M, A)){
-
-                A[c.i][c.j-1] = 2;
-
-                q.add( new cell(c.i, c.j-1) );
-
-                totalone--;
-
-            }
-            if( rotent(c.i, c.j+1, N, M, A)){
-
-                A[c.i][c.j+1] = 2;
-
-                q.add( new cell(c.i, c.j+1) );
-
-                totalone--;
-
-            }
-            if( rotent(c.i-1, c.j, N, M, A)){
-
-                A[c.i-1][c.j] = 2;
-
-                q.add( new cell(c.i-1, c.j) );
-
-                totalone--;
-
-            }
-            if( rotent(c.i+1, c.j, N, M, A)){
-
-                A[c.i+1][c.j] = 2;
-
-                q.add( new cell(c.i+1, c.j) );
-
-                totalone--;
-
-            }
-
-            }
-            
-
-            if(!q.isEmpty()){
-                time++;
-            }
-            
-
-        }
-
-
-        if(totalone==0){
-
-            return time;
-        }
-
-
-        return -1;
-
+        // Return time if all oranges are rotten; otherwise, return -1
+        return freshOranges == 0 ? time : -1;
     }
 }
