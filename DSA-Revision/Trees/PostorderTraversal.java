06134a9e718e325dc3e3868bae4e34217a57a9c7
qayyumma <qayyumemailid@gmail.com>
2024-11-29 18:47:06 +0530

    Create PostorderTraversal.java

---
+++ DSA-Revision/Trees/PostorderTraversal.java
@@ -0,0 +1,28 @@
+public void postorderTraversal(TreeNode root) {
+    if (root == null) return;
+    postorderTraversal(root.left);
+    postorderTraversal(root.right);
+    System.out.print(root.val + " ");
+}
+
+=====================================================
+
+
+public void postorderTraversalIterative(TreeNode root) {
+    if (root == null) return;
+
+    Stack<TreeNode> stack1 = new Stack<>();
+    Stack<TreeNode> stack2 = new Stack<>();
+    stack1.push(root);
+
+    while (!stack1.isEmpty()) {
+        TreeNode node = stack1.pop();
+        stack2.push(node);
+        if (node.left != null) stack1.push(node.left);
+        if (node.right != null) stack1.push(node.right);
+    }
+
+    while (!stack2.isEmpty()) {
+        System.out.print(stack2.pop().val + " ");
+    }
+}
