0787856bce0d22a9b8d32be01d13e045436279a9
qayyumma <qayyumemailid@gmail.com>
2025-01-01 16:33:02 +0530

    Update FractionalKnapsack.java

--- DSA-Revision/DynamicProgramming/FractionalKnapsack.java
+++ DSA-Revision/DynamicProgramming/FractionalKnapsack.java
@@ -1,67 +1,117 @@
-import java.util.*;
+Problem Description
 
-class Item {
-    int weight;
-    int value;
+Given two integer arrays A and B of size N each which represent values and weights associated with N items respectively.
 
-    Item(int weight, int value) {
-        this.weight = weight;
-        this.value = value;
-    }
-}
+Also given an integer C which represents knapsack capacity.
 
-class FractionalKnapsack {
-    static class ItemValue {
-        Double cost;
-        int weight, value;
+Find out the maximum total value that we can fit in the knapsack. If the maximum total value is ans, then return ⌊ans × 100⌋ , i.e., floor of (ans × 100).
 
-        // Constructor
-        public ItemValue(int weight, int value) {
-            this.weight = weight;
-            this.value = value;
-            this.cost = (double) value / weight; // value-to-weight ratio
+NOTE:
+
+You can break an item for maximizing the total value of the knapsack
+
+
+
+Problem Constraints
+
+1 <= N <= 105
+
+1 <= A[i], B[i] <= 103
+
+1 <= C <= 103
+
+
+
+
+Input Format
+
+First argument is an integer array A of size N denoting the values on N items.
+
+Second argument is an integer array B of size N denoting the weights on N items.
+
+Third argument is an integer C denoting the knapsack capacity.
+
+
+
+
+Output Format
+
+Return a single integer denoting the maximum total value of A such that sum of the weights of this subset is smaller than or equal to C.
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+public class Solution {
+
+
+    class Iteam{
+
+        int value;
+        int weight;
+        double ratio;
+
+        Iteam(int v, int w){
+
+            this.value = v;
+            this.weight = w;
+            this.ratio = (double)v/w;
         }
     }
 
-    public static double getMaxValue(int capacity, Item[] items) {
-        // Create a list of ItemValue objects
-        ItemValue[] itemValues = new ItemValue[items.length];
-        for (int i = 0; i < items.length; i++) {
-            itemValues[i] = new ItemValue(items[i].weight, items[i].value);
-        }
+    class customComparator implements Comparator<Iteam>{
 
-        // Sort items by cost in descending order
-        Arrays.sort(itemValues, (a, b) -> b.cost.compareTo(a.cost));
+        public int compare(Iteam i1, Iteam i2){
 
-        double totalValue = 0.0;
-
-        for (ItemValue item : itemValues) {
-            int currentWeight = item.weight;
-            int currentValue = item.value;
-
-            if (capacity >= currentWeight) {
-                // If the item can fit entirely, take it
-                capacity -= currentWeight;
-                totalValue += currentValue;
-            } else {
-                // Take the fraction of the item that fits
-                totalValue += item.cost * capacity;
-                break;
+            if(i1.ratio < i2.ratio ){
+                return 1;
             }
+            else if(i1.ratio >= i2.ratio){
+                return -1;
+            }
+            else{
+                return 0;
+            }
+
+
         }
 
-        return totalValue;
     }
 
-    public static void main(String[] args) {
-        Item[] items = {
-            new Item(10, 60),
-            new Item(20, 100),
-            new Item(30, 120)
-        };
-        int capacity = 50;
+    public int solve(int[] A, int[] B, int C) {
 
-        double maxValue = getMaxValue(capacity, items);
-        System.out.println("Maximum value we can obtain = " + maxValue);
+        Iteam[] iteams = new Iteam[A.length];
+
+        for(int i = 0; i < A.length; i++){
+
+            iteams[i] = new Iteam(A[i], B[i]);
+
+        }
+
+        Arrays.sort(iteams, new customComparator());
+
+        double ans = 0;
+
+        for(int i = 0; i < A.length; i++){
+
+            if(iteams[i].weight <= C){
+
+                ans += iteams[i].value;
+
+                C -= iteams[i].weight;
+
+            }
+            else{
+
+                ans += C * iteams[i].ratio;
+
+                break;
+
+            }
+
+        }
+
+        ans = (ans * 1000);
+
+        return (int) (ans/10);
+
     }
 }
