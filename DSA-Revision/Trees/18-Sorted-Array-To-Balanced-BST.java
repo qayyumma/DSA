76d03ae8754fc15dd2f5b2e3e7f0aaa264680823
qayyumma <qayyumemailid@gmail.com>
2025-01-11 22:52:47 +0530

    Update 18-Sorted-Array-To-Balanced-BST.java

--- DSA-Revision/Trees/18-Sorted-Array-To-Balanced-BST.java
+++ DSA-Revision/Trees/18-Sorted-Array-To-Balanced-BST.java
@@ -50,7 +50,7 @@ public class Solution {
             return null;
         }
 
-        int mid = start + (end - start) / 2 ;
+        int mid = start + (end) / 2 ;
 
         TreeNode root = new TreeNode(A[mid]);
 
