92b3a470f40f426a2397ab52daf25f52a537402d
qayyumma <qayyumemailid@gmail.com>
2025-01-12 21:04:09 +0530

    Update 6.Number-of-islands.java

--- DSA-Revision/Graphs/6.Number-of-islands.java
+++ DSA-Revision/Graphs/6.Number-of-islands.java
@@ -1,3 +1,37 @@
+Problem Description
+
+Given a matrix of integers A of size N x M consisting of 0 and 1. A group of connected 1's forms an island. From a cell (i, j) such that A[i][j] = 1 you can visit any cell that shares a corner with (i, j) and value in that cell is 1.
+
+More formally, from any cell (i, j) if A[i][j] = 1 you can visit:
+
+(i-1, j) if (i-1, j) is inside the matrix and A[i-1][j] = 1.
+(i, j-1) if (i, j-1) is inside the matrix and A[i][j-1] = 1.
+(i+1, j) if (i+1, j) is inside the matrix and A[i+1][j] = 1.
+(i, j+1) if (i, j+1) is inside the matrix and A[i][j+1] = 1.
+(i-1, j-1) if (i-1, j-1) is inside the matrix and A[i-1][j-1] = 1.
+(i+1, j+1) if (i+1, j+1) is inside the matrix and A[i+1][j+1] = 1.
+(i-1, j+1) if (i-1, j+1) is inside the matrix and A[i-1][j+1] = 1.
+(i+1, j-1) if (i+1, j-1) is inside the matrix and A[i+1][j-1] = 1.
+Return the number of islands.
+
+NOTE: Rows are numbered from top to bottom and columns are numbered from left to right.
+
+
+
+Problem Constraints
+1 <= N, M <= 100
+0 <= A[i] <= 1
+
+
+
+Input Format
+The only argument given is the integer matrix A.
+
+
+Output Format
+Return the number of islands.
+
+
 public class Solution {
     public int solve(int[][] A) {
 
@@ -46,3 +80,63 @@ public class Solution {
 
     }
 }
+
+
+=================================================================================================================================================
+
+
+public class Solution {
+
+    private int[][] directions = { {0, -1}, {0, 1}, {-1, 0}, {1, 0}, {-1, -1}, {-1, 1}, {1, -1}, {1, 1} };
+
+    private void dfs(int[][] A, int i, int j ){
+
+        A[i][j] = 2;
+
+        for(int[] dir: directions ){
+
+            int left = dir[0] + i;
+
+            int rigth = dir[1] + j;
+
+            if(isIsland( left, rigth, A)){
+
+                dfs(A, left, rigth);
+
+            }
+
+
+        }
+
+
+    }
+
+    private boolean isIsland( int i, int j, int[][] A ){
+
+        return i >= 0 && j >= 0 && i < A.length && j < A[i].length && A[i][j] == 1;
+
+    }
+
+    public int solve(int[][] A) {
+
+        int ans = 0;
+
+        for(int i = 0; i < A.length; i++){
+
+            for(int j = 0; j < A[i].length; j++){
+
+                if(A[i][j]==1){
+                    dfs(A, i, j);
+                    ans++;
+                }
+
+            }
+
+        }
+
+        return ans;
+
+    }
+}
+
+
