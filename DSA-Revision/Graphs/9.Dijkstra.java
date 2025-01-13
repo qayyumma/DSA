fe5c84eb2955adc92520c85580b99599375cefc1
qayyumma <qayyumemailid@gmail.com>
2025-01-13 11:56:51 +0530

    Update 9.Dijkstra.java

--- DSA-Revision/Graphs/9.Dijkstra.java
+++ DSA-Revision/Graphs/9.Dijkstra.java
@@ -1,3 +1,145 @@
+Problem Description
+
+Given a weighted undirected graph having A nodes and M weighted edges, and a source node C.
+
+You have to find an integer array D of size A such that:
+
+D[i]: Shortest distance from the C node to node i.
+If node i is not reachable from C then -1.
+Note:
+
+There are no self-loops in the graph.
+There are no multiple edges between two pairs of vertices.
+The graph may or may not be connected.
+Nodes are numbered from 0 to A-1.
+Your solution will run on multiple test cases. If you are using global variables, make sure to clear them.
+
+
+
+Problem Constraints
+
+1 <= A <= 1e5
+
+0 <= B[i][0],B[i][1] < A
+
+0 <= B[i][2] <= 1e3
+
+0 <= C < A
+
+
+
+Input Format
+
+The first argument is an integer A, representing the number of nodes in the graph.
+The second argument is a matrix B of size M x 3, where each row represents an edge in the graph. The three columns of each row denote the source node B[i][0], the destination node B[i][1], and the weight of the edge B[i][2].
+The third argument is an integer C, representing the source node for which the shortest distance to all other nodes needs to be found.
+
+
+Output Format
+
+Return the integer array D.
+=====================================================================================================================================
+
+
+public class Solution {
+    
+    class Pair{
+
+        int v;
+        int w;
+
+        public Pair(int v, int w){
+            this.v = v;
+            this.w = w;
+        }
+
+    }
+
+
+    boolean[] visited;
+
+    List<Pair>[] graph;
+
+    int[] ans;
+    
+    public int[] solve(int A, int[][] B, int C) {
+
+        visited = new boolean[A];
+
+        graph = new LinkedList[A];
+
+        ans = new int[A];
+
+        for(int i = 0; i < A; i++){
+            graph[i] = new LinkedList<>();
+            ans[i] = Integer.MAX_VALUE;
+        }
+
+        for(int i = 0; i < B.length; i++){
+
+            int u = B[i][0];
+
+            int v = B[i][1];
+
+            int w = B[i][2];
+
+            graph[u].add(new Pair(v, w));
+
+            graph[v].add(new Pair(u, w));
+
+        }
+
+        PriorityQueue<Pair> queue = new PriorityQueue<Pair>(new Comparator<Pair>() {
+            @Override
+            public int compare(Pair p1, Pair p2) {
+                return Integer.compare(p1.w, p2.w); // Min-heap based on weight
+            }
+        });
+
+        ans[C] = 0;
+
+        //visited[C] = true;
+
+        queue.add(new Pair(C, 0));
+
+        while(!queue.isEmpty()){
+
+            Pair temp = queue.poll();
+
+            if( temp.w > ans[temp.v]) continue;
+
+            for(Pair ele: graph[temp.v]){
+
+                int newW = temp.w + ele.w;
+
+                if(newW < ans[ele.v]){
+
+                    queue.add(new Pair(ele.v, newW));
+
+                    ans[ele.v] = newW;
+
+                }
+
+            }
+
+        }
+
+
+        for(int i = 0; i < A; i++){
+
+            if(ans[i]==Integer.MAX_VALUE){
+                ans[i] = -1;
+            }
+        }
+
+        return ans;
+
+    }
+}
+
+
+
+=======================================================================================================================================
 import java.util.*;
 
 public class Solution {
