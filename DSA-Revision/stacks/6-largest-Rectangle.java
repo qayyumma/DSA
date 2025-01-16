54f7fc95a077d3ae557b48986e60d8b4511af0a6
qayyumma <qayyumemailid@gmail.com>
2025-01-16 10:18:16 +0530

    Update and rename largestRectangle.java to 6-largest-Rectangle.java

--- DSA-Revision/stacks/6-largest-Rectangle.java
+++ DSA-Revision/stacks/6-largest-Rectangle.java
@@ -1,3 +1,41 @@
+Problem Description
+
+Given an array of integers A.
+
+A represents a histogram i.e A[i] denotes the height of the ith histogram's bar. Width of each bar is 1.
+
+Find the area of the largest rectangle formed by the histogram.
+
+Problem Constraints
+
+1 <= |A| <= 100000
+
+
+1 <= A[i] <= 10000
+
+
+Input Format
+The only argument given is the integer array A.
+
+
+Output Format
+Return the area of the largest rectangle in the histogram.
+
+Example Input
+Input 1:
+A = [2, 1, 5, 6, 2, 3]
+
+Input 2:
+A = [2]
+
+
+Example Output
+Output 1:
+ 10
+Output 2:
+ 2
+
+
 // Custom Stack Implementation
 class Stack<T> {
     private Node<T> top; // Top of the stack
