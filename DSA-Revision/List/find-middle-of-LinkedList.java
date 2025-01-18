b8de435a0460e3cbef33ea3bd8841c3aea362ed1
qayyumma <qayyumemailid@gmail.com>
2025-01-18 21:46:44 +0530

    Update find-middle-of-LinkedList.java

--- DSA-Revision/List/find-middle-of-LinkedList.java
+++ DSA-Revision/List/find-middle-of-LinkedList.java
@@ -1,3 +1,78 @@
+Problem Description
+
+Given a linked list of integers, find and return the middle element of the linked list.
+
+NOTE: If there are N nodes in the linked list and N is even then return the (N/2 + 1)th element.
+
+Problem Constraints
+
+1 <= length of the linked list <= 100000
+
+1 <= Node value <= 109
+
+Input Format
+The only argument given head pointer of linked list.
+
+
+Output Format
+Return the middle element of the linked list.
+
+
+Example Input
+
+Input 1:
+ 1 -> 2 -> 3 -> 4 -> 5
+Input 2:
+ 1 -> 5 -> 6 -> 2 -> 3 -> 4
+
+
+Example Output
+Output 1:
+ 3
+
+Output 2:
+ 2
+
+==================================================================================================
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public int solve(ListNode A) {
+
+        ListNode slow = A;
+
+        ListNode fast = A;
+
+        while(fast.next != null && fast.next.next != null){
+
+            slow = slow.next;
+
+            fast = fast.next.next;
+
+
+        }
+
+        if(fast.next != null){
+            return slow.next.val;
+        }
+
+        return slow.val;
+
+
+    }
+}
+
+
+===================================================================================================
+
 class LinkedList {
     Node head; // Head of the list
 
