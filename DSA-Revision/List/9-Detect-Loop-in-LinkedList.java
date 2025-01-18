97db7cec99f440e93f960532c10b910955f5cd61
qayyumma <qayyumemailid@gmail.com>
2025-01-18 23:35:53 +0530

    Update and rename Detect-Loop-in-LinkedList.java to 9-Detect-Loop-in-LinkedList.java

--- DSA-Revision/List/9-Detect-Loop-in-LinkedList.java
+++ DSA-Revision/List/9-Detect-Loop-in-LinkedList.java
@@ -1,3 +1,23 @@
+Problem Description
+
+You are using Google Maps to help you find your way around a new place. But, you get lost and end up walking in a circle. Google Maps has a way to keep track of where you've been with the help of special sensors.These sensors notice that you're walking in a loop, and now, Google wants to create a new feature to help you figure out exactly where you started going in circles.
+
+Here's how we can describe the challenge in simpler terms: You have a Linked List A that shows each step of your journey, like a chain of events. Some of these steps have accidentally led you back to a place you've already been, making you walk in a loop. The goal is to find out the exact point where you first started walking in this loop, and then you want to break the loop by not going in the circle just before this point.
+
+Problem Constraints
+
+1 <= number of nodes <= 1000
+
+Input Format
+
+The first of the input contains a LinkedList, where the first number is the number of nodes N, and the next N nodes are the node value of the linked list.
+The second line of the input contains an integer which denotes the position of node where cycle starts.
+
+Output Format
+return the head of the updated linked list.
+
+
+
 class Node {
     int data;
     Node next;
