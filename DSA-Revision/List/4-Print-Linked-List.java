5c7c710b9428683694fa25da4e404fcda763b0b7
qayyumma <qayyumemailid@gmail.com>
2025-01-18 21:38:12 +0530

    Create 4-Print-Linked-List.java

---
+++ DSA-Revision/List/4-Print-Linked-List.java
@@ -0,0 +1,60 @@
+Problem Description
+
+You are given A which is the head of a linked list. Print the linked list in space separated manner.
+
+Note : The last node value must also be succeeded by a space and after printing the entire list you should print a new line
+
+Problem Constraints
+1 <= size of linked list <= 105
+1 <= value of nodes <= 109
+
+
+Input Format
+The first argument A is the head of a linked list.
+
+Output Format
+You dont need to return anything
+
+
+Example Input
+Input 1:
+A = 1 -> 2 -> 3
+Input 2:
+A = 4 -> 3 -> 2 -> 1
+
+Example Output
+Output 1:
+1 2 3
+Output 2:
+4 3 2 1
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public void solve(ListNode A) {
+
+        while(A != null){
+
+            System.out.print(A.val);
+
+            A = A.next;
+
+            if(A != null);
+
+            System.out.print(" ");
+
+        }
+
+        System.out.println("");
+
+
+    }
+}
+
