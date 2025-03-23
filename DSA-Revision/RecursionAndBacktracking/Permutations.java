db7d688e31f7ad825faacdce657458ac9daf2030
qayyumma <qayyumemailid@gmail.com>
2025-03-23 08:23:05 +0530

    Update Permutations.java

--- DSA-Revision/RecursionAndBacktracking/Permutations.java
+++ DSA-Revision/RecursionAndBacktracking/Permutations.java
@@ -30,4 +30,58 @@ Return a 2-D array denoting all possible permutation of the array.
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+public class Solution {
+
+    int j = 0;
+
+    int[][] ans;    
+
+    public int[][] permute(int[] A) {
+
+        int N = 1;
+
+        for(int i = A.length; i > 1; i--){
+            N *= i;
+        }
+
+        ans = new int[N][A.length]; 
+
+        rb(A, 0);
+
+        return ans;       
+    }
+
+    private void rb(int[] A, int ind){
+
+        if(ind == A.length){
+
+            for(int i = 0; i < A.length; i++){
+                ans[j][i] =  A[i];
+            }
+
+            j = j+1;
+
+            return;
+            
+        }
+
+        for(int i = ind; i < A.length; i++){
+
+            swap( A, ind, i);
+            rb(A, ind+1);
+            swap(A, ind, i);
+
+
+        }
+
+
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+}
 
